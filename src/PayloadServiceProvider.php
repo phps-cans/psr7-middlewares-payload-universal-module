ea00e808767aae3ac4dcb7c3f7874e39bd34aed8
Benoît Ngô <ngobenoit@gmail.com>
2017-01-16 16:16:12 +0100

    Fixing namespace

--- src/PayloadServiceProvider.php
+++ src/PayloadServiceProvider.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Psr7Middlewares\Middleware;
+namespace PsCs\UniversalModule\Psr7Middlewares\Middleware;
 use Interop\Container\ServiceProvider;
 use Interop\Container\ContainerInterface as Container;
 
 class PayloadServiceProvider implements ServiceProvider {
   public function getServices() {
     return [
-        Payload::class => function(Container $container) {
+        \Psr7Middlewares\Middleware\Payload::class => function(Container $container) {
           return new Payload();
         }
     ];
